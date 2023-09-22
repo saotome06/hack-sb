1121fd0f1c8a9bb94fc132e2b6df2104aba6eff5
saotome06 <c01191306d@edu.teu.ac.jp>
2023-09-22 22:20:55 +0900

    カメラ起動ボタン作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+import { Box } from '@mui/material';
+import styles from './style.css';
 
 export default function Home() {
+  
   return (
-    <>
-      <input type="file" capture="environment" accept="image/*"></input>
-    </>
+    <Box
+      sx={{
+        width: '100%',
+        margin: 'auto',
+      }}
+    >
+      <label
+        style={{
+          border: '1px solid black',
+          padding: '10px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+          margin: 'auto',
+          width: '70%',
+          marginTop: '50%',
+        }}
+      >
+        <input type="file" capture="environment" accept="image/*" style={{display: 'none'}}></input>
+        カメラを起動
+      </label>
+    </Box>
   )
 }
